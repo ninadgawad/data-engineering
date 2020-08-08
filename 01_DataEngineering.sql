5d51ed74bb1f53ccf3cd47a5a43ad89de723f305
Ninad Gawad <ninadgawad@gmail.com>
2020-08-07 23:42:24 -0400

    Create 01_DataEngineering.sql

---
+++ 01_DataEngineering.sql
@@ -0,0 +1,32 @@
+// Create a Table using SQL
+CREATE TABLE UNIVERSE (  
+  galaxy_no     number,  
+  name          varchar2(50) not null, 
+  consellation  varchar2(50) not null, 
+  location      varchar2(50),  
+  constraint pk_departments primary key (galaxy_no)  
+);
+
+// Alter Table add age to Universe table 
+ALTER TABLE UNIVERSE 
+ADD AGE number(19,2);
+
+// Insert Data into Table
+INSERT INTO UNIVERSE (galaxy_no, name, consellation,location,age) 
+VALUES (1, 'MilkyWay', 'Sagittarius','Southeast',1310000000);
+
+
+// Delete from Universe Table all galaxies which have age as NULL
+DELETE FROM UNIVERSE
+WHERE AGE is null
+
+// Group Galaxies By consellation
+SELECT consellation, COUNT(*)
+FROM UNIVERSE
+GROUP BY consellation;
+
+// HAVING
+SELECT consellation, COUNT(*)
+FROM UNIVERSE
+GROUP BY consellation
+HAVING COUNT(*) > 10;
